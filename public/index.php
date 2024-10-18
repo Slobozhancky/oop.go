4ff78f9dcd45324ef77632380f5b79c23d6ab1ff
Slobozhancky <il.slob93@gmail.com>
2024-10-18 20:13:23 +0300

    feat:added getter and setter

--- public/index.php
+++ public/index.php
@@ -12,3 +12,6 @@ $worker_1 = new Developer('Tom', 42);
 
 $worker_1->work();
 
+$worker_1->setSalary(1000);
+
+var_dump($worker_1->getSalary());
--- src/Developer.php
+++ src/Developer.php
@@ -6,9 +6,31 @@ class Developer extends Worker
 {
   // TODO: 1. приклад того, як клас Developer, наслідує і переоприділяє клас Worker
   // А наслідує він властивості з конструктором з кладу Worker а в класі Developer перевизначає метод work
+
+
+  // TODO: 3. щоб задавати значення властивостям, з різним рівнем доступу, використовувати слід геттери та сеттери
+  // нижче приведено приклад, приватної властивості salary до якої не можна буде отримати доступу на екземплярі
+  // класу, тобто на обєкті, тому доступ щоб задати значення, або його тримати, будемо робити через геттер та сеттер
+  private float $salary;
   public function work ()
   {
     print_r("i`m {$this->workerName}, and me {$this->age}. And i`m developer");
+  }
+
+  /**
+   * @param float $salary
+   */
+  public function setSalary (float $salary) : void
+  {
+    $this->salary = $salary;
+  }
+
+  /**
+   * @return float
+   */
+  public function getSalary () : float
+  {
+    return $this->salary;
   }
 
 }
